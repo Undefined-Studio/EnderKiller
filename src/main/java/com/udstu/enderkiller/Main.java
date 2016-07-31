6065cf0167dd002a80916b4792fc647d20097825
PvtTony <yusongtao518@outlook.com>
2016-07-31 22:15:39 +0800

    Empty Spigot plugin

---
+++ src/main/java/com/udstu/enderkiller/Main.java
@@ -0,0 +1,23 @@
+package com.udstu.enderkiller;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by yst on 2016/7/31.
+ * Spigot main class
+ */
+public class Main extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable()
+    {
+        super.onDisable();
+    }
+}
+
