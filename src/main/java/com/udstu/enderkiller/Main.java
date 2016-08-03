dd8e9d5dbbf38da5d7a04e7dd9b157a04acf9ea4
czp <czp3009@gmail.com>
2016-08-04 02:03:14 +0800

    helloworld

--- src/main/java/com/udstu/enderkiller/Main.java
+++ src/main/java/com/udstu/enderkiller/Main.java
@@ -1,23 +1,21 @@
 package com.udstu.enderkiller;
 
+import com.udstu.enderkiller.command.CommandEK;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by yst on 2016/7/31.
  * Spigot main class
  */
-public class Main extends JavaPlugin
-{
+public class Main extends JavaPlugin {
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         super.onEnable();
+        this.getCommand("ek").setExecutor(new CommandEK());
     }
 
     @Override
-    public void onDisable()
-    {
+    public void onDisable() {
         super.onDisable();
     }
 }
-
