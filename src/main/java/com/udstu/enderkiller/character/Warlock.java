3653bcc4c6ff16dd3ee0299656c1c4efbee6ceb4
czp <czp3009@gmail.com>
2016-08-12 21:48:03 +0800

    增加角色类,具体职业类 增加阵营与职业的枚举类

---
+++ src/main/java/com/udstu/enderkiller/character/Warlock.java
@@ -0,0 +1,14 @@
+package com.udstu.enderkiller.character;
+
+import com.udstu.enderkiller.character.extend.Character;
+import com.udstu.enderkiller.enumeration.Alignment;
+
+/**
+ * Created by czp on 16-8-12.
+ * 巫师
+ */
+public class Warlock extends Character {
+    public Warlock(Character character, Alignment alignment) {
+        super(character, alignment);
+    }
+}
